ab37a97edc27e0f03e5a743b6770605fe224a02a
Michal Stawinski <michal.stawinski@gmail.com>
2015-08-21 00:57:03 +0200

    Select archspecific varian of libunwind, if available

--- src/deps/libbacktrace/cmake/FindUnwind.cmake
+++ src/deps/libbacktrace/cmake/FindUnwind.cmake
@@ -1,9 +1,18 @@
 
+
+set(UNWINDARCH unwind-${CMAKE_SYSTEM_PROCESSOR})
+
 find_path(LIBUNWIND_INCLUDE_DIR unwind.h)
 find_library(LIBUNWIND_LIBRARY NAMES unwind)
+find_library(LIBUNWIND_LIBRARY_ARCH NAMES ${UNWINDARCH})
 find_library(LIBUNWIND_PTRACE_LIBRARY NAMES unwind-ptrace)
 
-if(LIBUNWIND_INCLUDE_DIR AND LIBUNWIND_LIBRARY AND LIBUNWIND_PTRACE_LIBRARY)
+if(LIBUNWIND_INCLUDE_DIR AND LIBUNWIND_PTRACE_LIBRARY AND (LIBUNWIND_LIBRARY OR LIBUNWIND_LIBRARY_ARCH))
+   # assuming we're not cross-compiling, arch-specific lib is better
+   # hence overwrited the library name
+   if (LIBUNWIND_LIBRARY_ARCH)
+      set(LIBUNWIND_LIBRARY ${LIBUNWIND_LIBRARY_ARCH})
+   endif()
    set(UNWIND_FOUND TRUE)
 endif()
 
